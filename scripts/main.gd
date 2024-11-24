8d9b3688d4144c5762ae1db9871ba705ec7c107b
Edwin Wen <edwinwen75@gmail.com>
2024-11-23 22:09:28 -0500

    working y-sort

--- scripts/main.gd
+++ scripts/main.gd
@@ -7,20 +7,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-	#var viewport1 = Viewport.new()
-	#viewport1.size = Vector2(960, 1080)
-	#p1Camera.current = true
-	#p1Camera.get_viewport().add_child(viewport1)
-#
-	#var viewport2 = Viewport.new()
-	#viewport2.size = Vector2(960, 1080)
-	#p2Camera.current = true
-	#p2Camera.get_viewport().add_child(viewport2)
-#
-	## Split screen layout
-	#viewport1.position = Vector2(0, 0)
-	#viewport2.position = Vector2(960, 0)
+	var viewport1 = $SubViewportContainer/SubViewport 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
